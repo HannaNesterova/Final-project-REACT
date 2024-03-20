4f16eb0f781b7e7da6ad9704f405bb81d40bc8b4
Hanna Nesterova <nesterova.hanna@gmail.com>
2024-03-20 17:27:26 +0100

    Try to fix the red error in the login form

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import { store } from './redux/store';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('Domain:', process.env.REACT_APP_AUTH0_DOMAIN);
+console.log('Client ID:', process.env.REACT_APP_AUTH0_CLIENT_ID);
 root.render(
   
   <React.StrictMode>
-    <Auth0Provider
-    domain="dev-126muqcm75g1qtb7.us.auth0.com"
-    clientId="hTS0sp0HSdbnjcMum5dbjqK0y44MU18x"
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
